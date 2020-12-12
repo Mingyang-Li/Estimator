c0775d2a32e656e46e816ed25165bf3b889d2066
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-12-12 13:42:58 +1300

    Initial tweaks of the original version

---
+++ src/components/UserForm.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import FormUserDetails from "./FormUserDetails";
+import FormClientRequirements from "./FormClientRequirements";
+import FormMCQ from "./FormMCQ";
+import Confirm from "./Confirm";
+import Success from "./Success";
+
+const useStyles = withStyles({
+  root: {
+    backgroundColor: "#4caf50",
+    borderRadius: 3,
+  },
+});
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: "",
+    scale: "",
+    usage: "",
+    ui: "",
+    totalPrice: 0,
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
+  };
+
+  // Go back to prev step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1,
+    });
+  };
+
+  // Handle fields change
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  // add prices for mcq requirements
+  calculatePrice = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const {
+      firstName,
+      lastName,
+      email,
+      occupation,
+      city,
+      bio,
+      scale,
+      usage,
+      ui,
+    } = this.state;
+
+    const values = {
+      firstName,
+      lastName,
+      email,
+      occupation,
+      city,
+      bio,
+      scale,
+      usage,
+      ui,
+    };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormClientRequirements
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <FormClientRequirements
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 4:
+        return (
+          <FormMCQ
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 5:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 6:
+        return <Success />;
+      default:
+        console.log("This is a multi-step form built with React.");
+    }
+  }
+}
+
+export default UserForm;
